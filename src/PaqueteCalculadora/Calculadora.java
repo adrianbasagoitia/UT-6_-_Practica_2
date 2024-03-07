0491e731e6f232c05798be1667bf7f36ccf22820
adrianbasagoitia <79417614+adrianbasagoitia@users.noreply.github.com>
2024-03-07 11:42:33 +0100

    Documentación del metodo sumar

--- src/PaqueteCalculadora/Calculadora.java
+++ src/PaqueteCalculadora/Calculadora.java
@@ -17,6 +17,13 @@ public class Calculadora{
   /* ***************************** Get & Set ******************************* */
 
   /* ******************************* Methods ******************************* */
+
+  /**
+   * Metodo que suma dos numeros enteros.
+   * @param n1 El primero de los sumandos.
+   * @param n2 El segundo de los sumandos.
+   * @return El resultado de la suma de los dos sumandos.
+   */
   public int sumar(int n1, int n2){
     return n1+n2;
   } // End metodo sumar
