8b519069539f485fd32c5daa8d83ad2c9d32e20e
adrianbasagoitia <79417614+adrianbasagoitia@users.noreply.github.com>
2024-03-07 10:44:28 +0100

    Añadir instancia de la clase Scanner

--- src/PaqueteCalculadora/Main.java
+++ src/PaqueteCalculadora/Main.java
@@ -2,10 +2,13 @@
 package PaqueteCalculadora;
 
 /* ******************************* Import ******************************** */
-
+import java.util.Scanner;
 
 /* ******************************* Class ********************************* */
 public class Main{
+  /* ****************************** Constant ******************************* */
+  public static Scanner scan = new Scanner(System.in);
+
   /* ******************************* Methods ******************************* */
   public static void main(String[]args){
     menu();
