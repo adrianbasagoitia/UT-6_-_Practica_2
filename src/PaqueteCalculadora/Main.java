28c020e3a827c12718c557c0bfabecd301201252
adrianbasagoitia <79417614+adrianbasagoitia@users.noreply.github.com>
2024-03-07 10:39:40 +0100

    Creación método menú

--- src/PaqueteCalculadora/Main.java
+++ src/PaqueteCalculadora/Main.java
@@ -8,9 +8,14 @@ package PaqueteCalculadora;
 public class Main{
   /* ******************************* Methods ******************************* */
   public static void main(String[]args){
-
+    menu();
   } // End Main
 
-/* *********************************************************************** */
+  /* *********************************************************************** */
+  public static void menu(){
+    /* Local variables */
 
+    /* Local code */
+
+  } // End menu
 } // End class
