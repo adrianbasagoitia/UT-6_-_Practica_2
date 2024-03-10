6b3f406cdf0c16b34afe1e9bf7c6608ac41a3ee4
adrianbasagoitia <79417614+adrianbasagoitia@users.noreply.github.com>
2024-03-10 13:58:38 +0100

    Implementación y documentación del método de pruebas pedirNumero

--- test/PaqueteCalculadora/MainTest.java
+++ test/PaqueteCalculadora/MainTest.java
@@ -1,7 +1,9 @@
 package PaqueteCalculadora;
 
 import org.junit.jupiter.api.Test;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.io.InputStream;
+import java.io.ByteArrayInputStream;
 class MainTest {
 
   @org.junit.jupiter.api.BeforeEach
@@ -12,7 +14,25 @@ class MainTest {
   void tearDown() {
   }
 
+  /**
+   * Metodo de prueba que comprobara la validez del metodo pedirNumero
+   * de la clase Main.
+   */
   @Test
   void pedirNumero() {
-  }
+    /* Local variables */
+    String numTexto = "-4";
+
+    /* Local code */
+    /* Hacer un mock de la clase Scanner a traves de System.in
+     * con un Input Stream */
+    InputStream in = new ByteArrayInputStream(numTexto.getBytes());
+    System.setIn(in);
+
+    /* Llamar al metodo de prueba */
+    int num = Main.pedirNumero("Primero");
+
+    /* Comprobar el resultado */
+    assertEquals(Integer.parseInt(numTexto), num);
+  } // End pedirNumero
 }
