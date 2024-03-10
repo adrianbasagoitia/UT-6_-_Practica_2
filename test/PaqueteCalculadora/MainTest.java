f866b34611465e2e4687b8dae041381145cff2e6
adrianbasagoitia <79417614+adrianbasagoitia@users.noreply.github.com>
2024-03-10 12:44:21 +0100

    Creación del fichero de pruebas de la clase Main

---
+++ test/PaqueteCalculadora/MainTest.java
@@ -0,0 +1,18 @@
+package PaqueteCalculadora;
+
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+  @org.junit.jupiter.api.BeforeEach
+  void setUp() {
+  }
+
+  @org.junit.jupiter.api.AfterEach
+  void tearDown() {
+  }
+
+  @Test
+  void pedirNumero() {
+  }
+}
