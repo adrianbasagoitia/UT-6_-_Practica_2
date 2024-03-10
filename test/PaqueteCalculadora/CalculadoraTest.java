b70d35ffad034a931f150553a4f7b7a6d15be9be
adrianbasagoitia <79417614+adrianbasagoitia@users.noreply.github.com>
2024-03-10 14:14:18 +0100

    Implementación y documentación del método de prueba sumarTest

--- test/PaqueteCalculadora/CalculadoraTest.java
+++ test/PaqueteCalculadora/CalculadoraTest.java
@@ -2,16 +2,33 @@ package PaqueteCalculadora;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculadoraTest {
-
+  /* Local variables */
+  private final Calculadora c = new Calculadora();
   @BeforeEach
   void setUp() {
+
   }
 
   @AfterEach
   void tearDown() {
   }
-}
+
+  /**
+   * Metodo parametrizado para comprobar el correcto funcionamiento del metodo
+   * sumar de la clase Calculadora
+   * @param n1 Primero de los sumandos.
+   * @param n2 Segundo de los sumandos.
+   * @param res Resultado esperado de la operacion.
+   */
+  @ParameterizedTest(name = "El resultado de la suma de {0} y {1} es {2}")
+  @CsvSource({"0,1,1", "100,-500, -400","485, 15, 500"})
+  public void sumarTest(int n1, int n2, int res){
+    assertEquals(res, c.sumar(n1,n2));
+  } // End sumarTest
+} // End Class Calculadora Test
